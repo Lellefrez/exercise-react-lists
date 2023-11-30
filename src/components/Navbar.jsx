7f15ffba34d61d6a4a4714af19d0cac3fab30284
Manuel Frezzato <manuel.frezzato@gmail.com>
2023-11-30 19:32:22 +0100

    feat | creazione Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <figure>
+        <img
+          src="https://as2.ftcdn.net/v2/jpg/00/97/48/53/1000_F_97485384_cPtgCSIlgWT10NNtSt72VxByUwd3YZK1.jpg"
+          alt="Logo"
+        />
+      </figure>
+      <menu>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#"> Foods</a>
+        </li>
+        <li>
+          <a href="#">Lifestyle</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </menu>
+    </nav>
+  );
+};
+
+export default Navbar;
